806b7928fe53e0423952fbba1928d13432b892a2
Orlando Verdin <git@orli.land>
2024-10-13 16:12:17 -0700

    get resources elements

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # La Verdadera Historia Del Cine Mexicano
 
 Este proyecto tiene la finalidad de listar las distintas películas, asi como imágenes de los actores que se mencionan durante la serie del cine mexicano en el podcast **La Verdadera Historia de México**.
+
+## Formato JSON para datos
+
+```json
+{
+  "id": "",
+  "title": "",
+  "type": "",
+  "thumbnail": "",
+  "episode": "",
+  "time": "",
+  "description": ""
+}
+```
--- src/Masonry.jsx
+++ src/Masonry.jsx
@@ -1,19 +1,47 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import MiniMasonry from "minimasonry";
 import "./Masonry.css";
 
 const Masonry = () => {
   const masonryDiv = useRef();
+  const [episode, setEpisode] = useState("all");
+  const [resources, setResources] = useState([]);
 
   useEffect(() => {
+    getMovieData();
+
+    async function getMovieData() {
+      const res = await fetch("/data.json");
+      let data = await res.json();
+      if (episode === "all") {
+        setResources(data);
+      } else {
+        data.filter((element) => element.episode == episode);
+        setResources(data);
+      }
+    }
+
     const miniMasonry = new MiniMasonry({
       container: masonryDiv.current,
-      gutter: 16,
+      gutter: 24,
     });
-  }, []);
+  }, [episode]);
 
-  return <main className="masonry" ref={masonryDiv}></main>;
+  return (
+    <main className="masonry" ref={masonryDiv}>
+      {resources.map((element) => {
+        return (
+          <img
+            className="masonry__item"
+            src={element.thumbnail}
+            alt={element.title}
+            key={element.id}
+          />
+        );
+      })}
+    </main>
+  );
 };
 
 export default Masonry;
