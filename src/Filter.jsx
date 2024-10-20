ef0921fd9ea10bcecc37b64dea25ee980cfdc67c
Orlando Verdin <git@orli.land>
2024-10-20 13:02:48 -0700

    add filter component

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import "./Filter.css";
 
 const Filter = () => {
+  const [showOptions, setShowOptions] = useState(false);
+  const episodes = ["Episodio 1"];
+
   return (
     <form onSubmit={(e) => e.preventDefault()} className="filter">
-      <label htmlFor="selectEpisode" className="filter__label">
-        <select id="selectEpisode" className="filter__select">
-          <option value="1">Episode 1</option>
-        </select>
-      </label>
+      <button
+        className="filter__select"
+        onClick={() => setShowOptions(!showOptions)}
+      >
+        Episodio 1
+      </button>
+      <ul
+        className={`filter__options ${
+          !showOptions && "filter__options--hidden"
+        }`}
+      >
+        {episodes.map((episode, key) => {
+          return (
+            <li
+              className="filter__option"
+              key={key}
+              onClick={() => setShowOptions(!showOptions)}
+            >
+              {episode}
+            </li>
+          );
+        })}
+      </ul>
     </form>
   );
 };
